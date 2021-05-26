88f215567f43dc819ee3d5171833767707487aac
baggio4ever <baggio4ever@gmail.com>
2021-05-26 10:21:39 +0900

    opencv.js 導入してみた

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component, RendererFactory2 } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+declare var cv: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'enjoy-camera';
-  version = '0.0.9';
+  version = '0.1.1';
+
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit");
+
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = './assets/opencv/opencv452.js';
+    script.onload = () => {
+      console.log("loaded!");
+
+      if (cv.getBuildInformation) {
+        console.log(cv.getBuildInformation());
+      }
+    }
+
+    const div = document.getElementById('script');
+    div.insertAdjacentElement('afterend', script);
+  }
 
   getRouterLinkClasses(rla): string {
     var ret = [];
--- src/app/joys/opencv/opencv.component.ts
+++ src/app/joys/opencv/opencv.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+
+declare var cv: any;
 
 @Component({
   selector: 'app-opencv',
   templateUrl: './opencv.component.html',
   styleUrls: ['./opencv.component.css']
 })
-export class OpencvComponent implements OnInit {
+export class OpencvComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('imageSrc0') imageSrc: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+
+  }
+
+  onChange(ev) {
+    console.log('changed!');
+    //let imgElement = document.getElementById("imageSrc")
+    this.imageSrc.nativeElement.src = URL.createObjectURL(ev.target.files[0]);
+  }
+
+  onClick1() {
+    console.log('clicked1!');
+
+    let mat = cv.imread(this.imageSrc.nativeElement);
+    cv.imshow("outputCanvas", mat);
+    mat.delete();
+  }
+
+  onClick2() {
+    console.log('clicked2');
+
+    let src = cv.imread('outputCanvas');
+    let dst = new cv.Mat();
+    cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
+    cv.imshow('monoCanvas', dst);
+    src.delete();
+    dst.delete();
+  }
 }
