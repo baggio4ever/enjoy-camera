f82aadf312485fb66319696ab506eac3ffebeac2
baggio4ever <baggio4ever@gmail.com>
2021-05-26 17:29:31 +0900

    iphoneでカメラ映るようになった！ playsinline 属性が効いた雰囲気。

--- src/app/joys/opencv2/opencv2.component.ts
+++ src/app/joys/opencv2/opencv2.component.ts
@@ -20,6 +20,35 @@ export class Opencv2Component implements OnInit {
   }
 
   onClick() {
+    /*
+    let iphoneConfig = {
+      video: { facingMode: { exact: "environment" } },
+      audio: false
+    };
+    let normalConfig = {
+      video: {
+        deviceId: this.targetVideoInputId
+      },
+      audio: false
+    };
+
+    let config;
+    if (this.isSmartPhone()) {
+      config = iphoneConfig;
+    } else {
+      config = normalConfig;
+    }
+
+    navigator.mediaDevices.getUserMedia(
+      config
+    ).then(
+      (stream) => {
+        this.video.nativeElement.srcObject = stream;
+        //this.video.nativeElement.play();
+      }
+    )
+    */
+/*
     navigator.mediaDevices.enumerateDevices().then((devices) => {
       console.log(devices);
       this.videoInputs = [];
@@ -39,10 +68,11 @@ export class Opencv2Component implements OnInit {
         this.targetVideoInputId = this.videoInputs[0].id;
       }
     });
-
+*/
     console.log(this.iamSmartPhone());
   }
 
+  errLog='errLog';
   streaming = false;
   onClick3() {
     console.log('clicked3');
@@ -57,20 +87,67 @@ export class Opencv2Component implements OnInit {
       },
       audio: false
     };
+
     let config;
     if (this.isSmartPhone()) {
       config = iphoneConfig;
     } else {
       config = normalConfig;
     }
+
     navigator.mediaDevices.getUserMedia(
       config
     ).then(
       (stream) => {
+        console.log('getUserMedia().then()');
         this.video.nativeElement.srcObject = stream;
-        this.video.nativeElement.play();
+        //this.video.nativeElement.play();
+        //this.startOpenCv();
+        //console.log('width: '+this.video.nativeElement.width);
+        //console.log('height: '+this.video.nativeElement.height);
+        this.errLog = 'Yes!!';
       }
-    )
+    ).catch(
+      (err) => {
+        console.log(err.name + ': '+ err.message);
+        this.errLog = JSON.stringify(err);
+      }
+    );
+/*
+    let src = new cv.Mat(240, 320, cv.CV_8UC4);
+    let dst = new cv.Mat(240, 320, cv.CV_8UC1);
+    let cap = new cv.VideoCapture(this.video.nativeElement);
+
+    const FPS = 30;
+    let processVideo = () => {
+      //console.log('yes!!!');
+      if (!this.streaming) {
+        if (src) {
+          src.delete();
+        }
+        if (dst) {
+          dst.delete();
+        }
+        return;
+      }
+      let begin = Date.now();
+      cap.read(src);
+      cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
+      cv.flip(dst, dst, 0);
+      cv.imshow('videoOutput', dst);
+      let delay = 1000 / FPS - (Date.now() - begin);
+      setTimeout(processVideo, delay);
+    }
+    setTimeout(() => {
+      console.log('yes!');
+      this.streaming = true;
+      processVideo();
+    }, 0);
+    */
+  }
+
+  startOpenCv() {
+    console.log('startOpenCv()');
 
     let src = new cv.Mat(240, 320, cv.CV_8UC4);
     let dst = new cv.Mat(240, 320, cv.CV_8UC1);
